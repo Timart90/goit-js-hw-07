90229af4cc9e2995bfc528b79c2fe31ac673a212
Timart90 <118985642+Timart90@users.noreply.github.com>
2023-08-06 16:46:17 +0300

    task 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+function createrImg (arr) {
+  return arr.map(({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('')
+
+};
+const ul = document.querySelector('.gallery');
+
+ul.addEventListener('click', handlerClick);
+ul.insertAdjacentHTML('beforeend', createrImg(galleryItems));
+
+
+function handlerClick(e) {
+  e.preventDefault();
+const fullImg = e.target.dataset.source;
+const instance = basicLightbox.create(`
+    <img src="${fullImg}" width="800" height="600">`)
+  instance.show();
+  
+  ul.addEventListener('keydown', (e) => {
+  if (e.code === 'Escape') {
+    instance.close();
+  }
+});
+};
+
+
+
